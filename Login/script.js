530bbd4abf8ec94233528b2a9a6a9bf4ac316eb1
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-03-10 16:20:58 -0300

    function close modal

---
+++ Login/script.js
@@ -0,0 +1,27 @@
+function login(){
+    var nome = $("#nome").val()
+    var senha = $("#senha").val()
+
+    if(nome && senha && nome === "admin" && senha === "1234"){
+        const user = {
+            name: nome,
+            dataEntrada: new Date(),
+            id: Math.floor(Math.random() * 100000),
+        }
+
+        localStorage.setItem("usuario", JSON.stringify(user))
+
+        window.location.href = "https://www.globo.com"
+    }else{
+        document.getElementById("error-modal").style.display = "flex"
+        document.getElementById("nome").style.border = "2px solid lightpink"
+        document.getElementById("senha").style.border = "2px solid lightpink"
+    }
+}
+
+function fecharError(){
+    document.getElementById("nome").style.border = "2px solid aliceblue"
+    document.getElementById("senha").style.border = "2px solid aliceblue"
+    document.getElementById("error-modal").style.display = "none"
+}
+
